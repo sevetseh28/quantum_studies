5d4f3f846b8c36a48f91a847150a38a1ca50c269
Hernan Esteves <hernan.esteves@codility.com>
2025-06-10 16:47:40 +0000

    more barrier examples

--- quantum-studies/src/quantum_studies/qiskit_circuits/basics/barrier_example.py
+++ quantum-studies/src/quantum_studies/qiskit_circuits/basics/barrier_example.py
@@ -1,13 +1,15 @@
 from qiskit import QuantumCircuit
 
-qc = QuantumCircuit(2)
+qc = QuantumCircuit(5)
 qc.h([0, 1])
 qc.barrier()
 qc.x(0)
 qc.x(0)
 qc.s(1)
-qc.barrier([1])
-qc.barrier(1)
+qc.barrier([1], [4, 3])
+qc.barrier(0, 1)
 qc.barrier()
+qc.barrier(-1)
+qc.barrier([-1, -2])
 qc.s(1)
 print(qc.draw())  # Visualize the circuit
